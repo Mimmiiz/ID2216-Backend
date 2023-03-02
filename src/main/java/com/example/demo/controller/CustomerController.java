af788ed0b1c15c8f7b97e797c51b80cb99ce23cf
Oliver Schwalbe Lehtihet <sl.oliver@proton.me>
2023-03-02 11:15:29 +0100

    Added customer DB functionallity

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,4 +1,30 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.Customer;
+import com.example.demo.service.CustomerService;
+
+@RestController
 public class CustomerController {
+
+    @Autowired private CustomerService customerService; 
+
+    @GetMapping("/customers")
+    public Customer getCustomer(@RequestParam(value = "id") Integer id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @GetMapping("/customers-all")
+    public List<Customer> getCustomer() {
+        return customerService.getCustomer();
+    }
+
+    @PostMapping("/customers-post")
+    public void save(@RequestBody Customer customer){
+        customerService.save(customer);
+    }
 }
