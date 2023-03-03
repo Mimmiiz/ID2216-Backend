75ccea59f771dcf0798c20e8bf3d82944924b80b
Maria Halvarsson <mahalv@kth.se>
2023-03-03 10:37:16 +0100

    Added return status and empty json

--- src/main/java/com/example/demo/controller/ServiceProfessionalController.java
+++ src/main/java/com/example/demo/controller/ServiceProfessionalController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.model.ServiceProfessional;
 import com.example.demo.service.ServiceProfessionalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,8 +26,13 @@ public class ServiceProfessionalController {
     }
 
     @PostMapping(value = "/saveserviceProfessional")
-    public void save(@RequestBody ServiceProfessional serviceprofessional){
-        serviceProfessionalService.save(serviceprofessional);
+    public ResponseEntity<String> save(@RequestBody ServiceProfessional serviceprofessional){
+        try {
+            serviceProfessionalService.save(serviceprofessional);
+        } catch (Exception e) {
+            return new ResponseEntity<>("{}", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("{}", HttpStatus.OK);
     }
 
 }
